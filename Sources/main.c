e09cd48bb39b93d6173df1edf88834fa6fa3301d
Juan Cavalieri <juanignaciocavalieri@gmail.com>
2019-01-03 21:52:39 -0300

    Cambio en la estructura del main.c

--- Sources/main.c
+++ Sources/main.c
@@ -2,20 +2,77 @@
 #include "Funciones.h"
 #include "Inicializaciones.h"
 
+//-------------- Vectores de señales ----------------
+
+#pragma DATA_SECTION(sweep, ".EXT_RAM")
+#pragma DATA_SECTION(left_ch, ".EXT_RAM")
+#pragma DATA_SECTION(right_ch, ".EXT_RAM")
+
+Complex sweep[N], left_ch[N], right_ch[N];
+
+//-------------- Variables auxiliares ---------------
 
 volatile unsigned int cuentas = 0;
 int j = 0;
+int puls_levantados = 1;
 
+//-------------- Cargar Sweep desde la SD -----------
+
+SD_init();
+//Cargar_sweep();
 
 void main(){
 
-	IRQ_globalDisable(); //Deshabilita todas las interrupciones
-	DSK6713_init(); //Inicializa DSK-EMIF,PLL
-	Timer_init(); //Inicializa el Timer
-	Interrup_init(); //Inicializa las interrupciones de
+	//-------------- Inicializaciones ----------------------
 
+	IRQ_globalDisable();
+	DSK6713_init();
+	Timer_init();
+	Interrup_init();
 
-	while(1) {}
+	//-------------- Bucle infinito ------------------------
+
+	while(1) {
+
+		//---------- Modo 1 --------------------------------
+		if(!DSK6713_DIP_get(0) && puls_levantados){
+			Codec_init();
+			//Play_codec();
+			//Corregir_resp_freq();
+			SD_init();
+			//Guardar_sweep();
+			puls_levantados = 0;
+		}
+
+		//---------- Modo 2 --------------------------------
+		if(!DSK6713_DIP_get(1) && puls_levantados){
+			Codec_init();
+			//Medir_ruido();
+			//while(Corregir_nivel == False)
+				//Play_test();
+			puls_levantados = 0;
+		}
+
+		//---------- Modo 3 --------------------------------
+		if(!DSK6713_DIP_get(2) && puls_levantados){
+			Codec_init();
+			//Play_codec();
+			//Obtener_RI();
+			puls_levantados = 0;
+		}
+
+		//---------- Modo 4 --------------------------------
+		if(!DSK6713_DIP_get(3) && puls_levantados){
+			SD_init();
+			//Guardar_RI();
+			puls_levantados = 0;
+		}
+
+		//Espera a que todos los pulsadores esten levantados para
+		//poder entrar en otro modo
+		if((DSK6713_DIP_get(0) && DSK6713_DIP_get(1) && DSK6713_DIP_get(2) && DSK6713_DIP_get(3)) == 1)
+			puls_levantados = 1;
+	}
 }
 
 interrupt void c_int11(){         //interrupt service routine
